6ac4e8e54b84d7251b7f71a3511df74cf7554297
j.sayegh <jadsay2@gmail.com>
2017-09-17 19:39:58 +0300

    Other Entities Resources
    Order, Order Detail, Order Detail Hisotry, Type & Status

---
+++ backend/src/main/java/com/cnam/valeurc/OderDetailResource.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cnam.valeurc;
+
+import com.cnam.valeurc.model.OrderDetail;
+import com.cnam.valeurc.service.OrderDetailService;
+import java.net.UnknownHostException;
+import java.util.*;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+/**
+ *
+ * @author KHATT
+ */
+@Path("orderDetails")
+public class OrderDetailResource {
+
+    OrderDetailService orderDetailService;
+
+    public OrderDetailResource() throws UnknownHostException {
+        this.orderDetailService = new OrderDetailService();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    
+    public List<OrderDetail> getOrderDetails() throws UnknownHostException {
+        return orderDetailService.getAllOrderDetails();
+    }
+
+    @GET
+    @Path("/{orderDetailId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    
+    public OrderDetail getOrderDetail(@PathParam("orderDetailId") String orderDetailId) throws UnknownHostException {
+
+        return orderDetailService.getOrderDetailById(orderDetailId);
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    
+    public OrderDetail addOrderDetail(OrderDetail orderDetail) throws UnknownHostException {
+        return orderDetailService.addOrderDetail(orderDetail);
+
+    }
+
+    @PUT
+    @Path("/{orderDetailId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    
+    public OrderDetail updateOrderDetail(OrderDetail orderDetail, @PathParam("orderDetailId") String orderDetailId) throws UnknownHostException {
+        return orderDetailService.updateOrderDetail(orderDetail, orderDetailId);
+
+    }
+
+    @DELETE
+    @Path("/{orderDetailId}")
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public void deleteOrderDetail(@PathParam("orderDetailId") String orderDetailId) throws UnknownHostException {
+        orderDetailService.deleteOrderDetail(orderDetailId);
+
+    }
+
+}
