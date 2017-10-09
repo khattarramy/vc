fe7244b2e27325634da15ae14c554be97ae14908
j.sayegh <jadsay2@gmail.com>
2017-10-09 04:10:16 +0300

    Fix condition in the checkcounters function

--- backend/src/main/java/com/cnam/valeurc/AppUtils.java
+++ backend/src/main/java/com/cnam/valeurc/AppUtils.java
@@ -1,11 +1,10 @@
 package com.cnam.valeurc;
 
-import com.cnam.valeurc.model.User;
+
 import com.cnam.valeurc.service.DbConnect;
-import com.cnam.valeurc.service.UserService;
+
 import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
-
 import com.mongodb.DBObject;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
@@ -13,9 +12,10 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.CreateCollectionOptions;
 import static com.mongodb.client.model.Filters.eq;
 import com.mongodb.util.JSON;
-import java.net.UnknownHostException;
 import org.bson.Document;
-
+import java.net.UnknownHostException;
+import com.cnam.valeurc.model.*;
+import com.cnam.valeurc.service.*;
 /**
  * @author Igor Baiborodine
  */
@@ -51,43 +51,40 @@ public class AppUtils {
         String json = new Gson().toJson(pojo);
         return (BasicDBObject) JSON.parse(json);
     }
- 
+
     public static Object getNextSequence(String name, MongoCollection collection) throws Exception {
-        collection.updateOne(eq("_id", name),  new Document("$inc", new Document("seq", 1)) );
+        collection.updateOne(eq("_id", name), new Document("$inc", new Document("seq", 1)));
         Document myDoc = (Document) collection.find(eq("_id", name)).first();
         return myDoc.get("seq");
     }
-    
-    public static MongoCollection checkCounters(DbConnect dbConnect,MongoDatabase db,String counter_id) {
+
+    public static MongoCollection checkCounters(DbConnect dbConnect, MongoDatabase db, String counter_id) {
         MongoCollection counters;
         if (!dbConnect.collectionExists("counters")) {
             db.createCollection("counters", new CreateCollectionOptions().capped(false));
-            counters = db.getCollection("counters");
-            BasicDBObject searchQuery = new BasicDBObject();
-            searchQuery.put("_id", counter_id);
-            MongoCursor<Document> cursor = counters.find(eq("_id", counter_id)).iterator();
-            if (!cursor.hasNext()) {
-                String json =  String.format("{'_id' : '%s','seq' : 0}", counter_id);
-                Document doc = AppUtils.JSONtoDocument(json);
-                counters.insertOne(doc);
-            }
-        } else {
-            counters = db.getCollection("counters");
+        }
+        counters = db.getCollection("counters");
+        BasicDBObject searchQuery = new BasicDBObject();
+        searchQuery.put("_id", counter_id);
+        MongoCursor<Document> cursor = counters.find(eq("_id", counter_id)).iterator();
+        if (!cursor.hasNext()) {
+            String json = String.format("{'_id' : '%s','seq' : 0}", counter_id);
+            Document doc = AppUtils.JSONtoDocument(json);
+            counters.insertOne(doc);
         }
         return counters;
     }
-//
+
 //    public static void main(String[] args) throws Exception {
 //        try {
-//            UserService userServ = new UserService();
-////            User user = new User();
-////            user.setName("name12");
-////            user.setAddress("address12");
-////            user.setPhone("12324512");
-////            user.setEmail("email12");
-////            user.setPassword("password12");
-////            user.setType("retailer2");
-////            userServ.addUser(user);
+//            ItemService itemServ = new ItemService();
+//            Item item = new Item();
+//            item.setName("name12");
+//            item.setDescription("address12");
+//            item.setDistributorId(12);
+//            item.setManufacturerId(22);
+//            item.setModelNumber("password12");
+//            itemServ.addItem(item);
 ////                        userServ.addUser(user);
 ////            userServ.addUser(user);
 ////            userServ.addUser(user);
@@ -96,7 +93,7 @@ public class AppUtils {
 ////            userServ.addUser(user);
 ////            userServ.addUser(user);
 //
-//            userServ.deleteUser(6);
+////            userServ.deleteUser(6);
 //
 //        } catch (UnknownHostException e) {
 //            e.printStackTrace();
