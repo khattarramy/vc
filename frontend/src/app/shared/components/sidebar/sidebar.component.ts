e80ba1318db6445874e79218a9ae358bdbfdb4e5
RidaAli <ali.rd93@gmail.com>
2017-10-15 17:02:59 +0300

    Get current logged in user information in sidebar component

--- frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'app/login/user.model';
 
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+    user: User = new User();
     isActive = false;
     showMenu = '';
     eventCalled() {
         this.isActive = !this.isActive;
     }
+
+    ngOnInit() {
+
+        this.user.email = localStorage.getItem('email');
+        this.user.password = localStorage.getItem('password');
+        this.user.userId = parseInt(localStorage.getItem('userId'));
+        this.user.type = localStorage.getItem('type');
+    }
+
     addExpandClass(element: any) {
         if (element === this.showMenu) {
             this.showMenu = '0';
