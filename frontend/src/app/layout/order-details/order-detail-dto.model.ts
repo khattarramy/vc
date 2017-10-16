1de568fc28c524a5f2d26ff5fe796af7af3afff4
MireilleKhalifeh <MireilleKhalifeh@Mireille-Khalifeh.local>
2017-10-16 18:15:33 +0300

    Updated orderDto and orderDetailDto

--- frontend/src/app/layout/order-details/order-detail-dto.model.ts
+++ frontend/src/app/layout/order-details/order-detail-dto.model.ts
@@ -3,17 +3,19 @@ export class OrderDetailDto {
   public orderDetailId: Number;
   public orderId: Number;
   public status: string;
+  public itemId: Number;
   public itemName: string;
   public quantity : Number;
   public quantityDistributor : Number;
   
 
 
-  constructor(orderDetailId: Number=0, orderId: Number=0, itemName: string="", 
+  constructor(orderDetailId: Number=0, orderId: Number=0, itemId:Number = 0 ,itemName: string="", 
     status: string="",quantity : Number=0,quantityDistributor : Number=0) {
     this.orderId = orderId;
     this.status = status;
     this.itemName = itemName;
+    this.itemId = itemId
     this.orderDetailId = orderDetailId;
     this.quantity = quantity;
     this.quantityDistributor = quantityDistributor;
