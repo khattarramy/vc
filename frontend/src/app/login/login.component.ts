ecb0fd6230f12f96d84e944625b54f46399a0ade
RidaAli <ali.rd93@gmail.com>
2017-10-20 16:32:24 +0300

    Updated sample data

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -43,33 +43,46 @@ export class LoginComponent implements OnInit {
   ngOnInit() { }
 
   addSampleData() {
-    this.userService.deleteUsers().subscribe(response =>{
-      this.users.push(new User("retailer", "Retailer1 User", "Beirut", "03000000", "retailer1@isae.edu.lb", "retailer1"));
-      this.users.push(new User("retailer", "Retailer2 User", "Beirut", "03111111", "retailer2@isae.edu.lb", "retailer2"));
-      this.users.push(new User("retailer", "Retailer3 User", "Beirut", "03222222", "retailer3@isae.edu.lb", "retailer3"));
-  
-      this.users.push(new User("distributor", "Distributor1 User", "Beirut", "03000000", "distributor1@isae.edu.lb", "distributor1"));
-      this.users.push(new User("distributor", "Distributor2 User", "Beirut", "03111111", "distributor2@isae.edu.lb", "distributor2"));
-      this.users.push(new User("distributor", "Distributor3 User", "Beirut", "03222222", "distributor3@isae.edu.lb", "distributor3"));
-  
-      this.users.push(new User("manufacturer", "Manufacturer1 User", "Beirut", "03000000", "manufacturer1@isae.edu.lb", "manufacturer1"));
-      this.users.push(new User("manufacturer", "Manufacturer2 User", "Beirut", "03111111", "manufacturer2@isae.edu.lb", "manufacturer2"));
-      this.users.push(new User("manufacturer", "Manufacturer3 User", "Beirut", "03222222", "manufacturer3@isae.edu.lb", "manufacturer3"));
-  
-      this.users.push(new User("admin", "admin User", "Beirut", "03222222", "admin@isae.edu.lb", "admin"));
-  
+    this.userService.deleteUsers().subscribe(response => {
+    /* id: 1  */    this.users.push(new User("retailer", "Khoury Home", "Beirut", "03000000", "khouryhome@isae.edu.lb", "khouryhome"));
+    /* id: 2  */    this.users.push(new User("retailer", "Abed Tahan", "Beirut", "03111111", "abedtahan@isae.edu.lb", "abedtahan"));
+    /* id: 3  */    this.users.push(new User("retailer", "Class", "Beirut", "03222222", "class@isae.edu.lb", "class"));
+    /* id: 4  */    this.users.push(new User("retailer", "CNAM", "Beirut", "03222222", "cnam@isae.edu.lb", "cnam"));
+    /* id: 5  */    this.users.push(new User("distributor", "Samsung Distributor", "Beirut", "03000000", "samsungdistributor@isae.edu.lb", "samsungdistributor"));
+    /* id: 6  */    this.users.push(new User("distributor", "LG Distributor", "Beirut", "03111111", "lgdistributor@isae.edu.lb", "lgdistributor"));
+    /* id: 7  */    this.users.push(new User("distributor", "Sony Distributor", "Beirut", "03222222", "sonydistributor@isae.edu.lb", "sonydistributor"));
+    /* id: 8  */    this.users.push(new User("distributor", "HP Distributor", "Beirut", "03000000", "hpdistributor@isae.edu.lb", "hpdistributor"));
+    /* id: 9  */    this.users.push(new User("distributor", "Lenovo Distributor", "Beirut", "03111111", "lenovodistributor@isae.edu.lb", "lenovodistributor"));
+    /* id: 10 */    this.users.push(new User("distributor", "Campomatic Distributor", "Beirut", "03222222", "campomaticdistributor@isae.edu.lb", "campomaticdistributor"));
+    /* id: 11 */    this.users.push(new User("distributor", "Dell Distributor", "Beirut", "03222222", "delldistributor@isae.edu.lb", "delldistributor"));
+    /* id: 12 */    this.users.push(new User("manufacturer", "Samsung Manufacturer", "Beirut", "03000000", "samsungmanufacturer@isae.edu.lb", "samsungmanufacturer"));
+    /* id: 13 */    this.users.push(new User("manufacturer", "LG Manufacturer", "Beirut", "03111111", "lgmanufacturer@isae.edu.lb", "lgmanufacturer"));
+    /* id: 14 */    this.users.push(new User("manufacturer", "Sony Manufacturer", "Beirut", "03222222", "sonymanufacturer@isae.edu.lb", "sonymanufacturer"));
+    /* id: 15 */    this.users.push(new User("manufacturer", "HP Manufacturer", "Beirut", "03000000", "hpmanufacturer@isae.edu.lb", "hpmanufacturer"));
+    /* id: 16 */    this.users.push(new User("manufacturer", "Lenovo Manufacturer", "Beirut", "03111111", "lenovomanufacturer@isae.edu.lb", "lenovomanufacturer"));
+    /* id: 17 */    this.users.push(new User("manufacturer", "Campomatic Manufacturer", "Beirut", "03222222", "campomaticmanufacturer@isae.edu.lb", "campomaticmanufacturer"));
+    /* id: 18 */    this.users.push(new User("manufacturer", "Dell Manufacturer", "Beirut", "03222222", "dellmanufacturer@isae.edu.lb", "dellmanufacturer"));
+    /* id: 19 */    this.users.push(new User("admin", "admin User", "Beirut", "03222222", "admin@isae.edu.lb", "admin"));
+
       this.addUsers(0, this.users);
-  
+
     });
 
-    this.itemService.deleteItems().subscribe(response =>{
-      this.items.push(new Item("TV","Samsung LED TV","9qpfjpqwf",4,5));
-      this.items.push(new Item("Phone","Samsung S7","afsa3f3f",4,7));
-      this.items.push(new Item("Laptop","Lenovo Laptop","33fasfasf",6,8));
-      
-      this.addItems(0,this.items);
+    this.itemService.deleteItems().subscribe(response => {
+      this.items.push(new Item("Samsung Smart TV 50 inch", "", "", 5, 12));
+      this.items.push(new Item("LG Refrigerator", "", "", 6, 13));
+      this.items.push(new Item("Campomatic washing machine", "", "", 10, 17));
+      this.items.push(new Item("Campomatic air conditioner", "", "", 10, 17));
+      this.items.push(new Item("Sony home theatre", "", "", 7, 14));
+      this.items.push(new Item("HP Desk pro 3600", "", "", 8, 15));
+      this.items.push(new Item("Lenovo thinkpad", "", "", 9, 16));
+      this.items.push(new Item("Samsung galaxy S8", "", "", 5, 12));
+      this.items.push(new Item("LG TV 55 inch", "", "", 6, 13));
+      this.items.push(new Item("HP LaserJet Printer 5001", "", "", 8, 15));
+      this.items.push(new Item("Dell XPS", "", "", 11, 18));
+      this.addItems(0, this.items);
     });
-    
+
 
     this.OrderService.deleteOrders().subscribe();
     this.orderDetailService.deleteOrderDetails().subscribe();
